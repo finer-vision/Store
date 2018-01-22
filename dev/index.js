874e110c876c73785d9c5471b914f216c75b9857
James Craig <jamescraig@finervision.com>
2018-01-22 08:09:25 +0000

    Added dev examples

--- dev/index.js
+++ dev/index.js
@@ -1,11 +1,21 @@
 import Store from "../src";
 
-const subscription = Store.subscribe('update', state => {
-    console.log('state was updated', state);
+Store.set({counter: 0});
+
+const counter = document.querySelector('#counter');
+const increment = document.querySelector('#increment');
+const decrement = document.querySelector('#decrement');
+
+increment.addEventListener('click', () => {
+    Store.set({counter: Store.get('counter') + 1});
+    counter.innerText = Store.get('counter');
 });
 
-Store.set({example: 'Example store value'});
+decrement.addEventListener('click', () => {
+    Store.set({counter: Store.get('counter') - 1});
+    counter.innerText = Store.get('counter');
+});
 
-console.log(Store.get('example'));
-
-Store.unSubscribe(subscription);
+Store.subscribe('update', state => {
+    console.log('state was updated', state);
+});
